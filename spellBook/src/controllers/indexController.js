c76d062219529d4ce918f282274ab852fc09150c
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2021-08-14 10:57:39 -0300

    se hace dinamica la vista novelties

--- spellBook/src/controllers/indexController.js
+++ spellBook/src/controllers/indexController.js
@@ -7,9 +7,12 @@ module.exports={
     books: (req, res) => { res.render('products/books')},
     novelties: (req, res) => { 
         let booksNovelties = products.slice(products.length-4);
-      /*   res.send(booksNovelties) */
+        let booksRecommended = products.filter(product => product.seleccionadoMes == 1);
+
+     
         res.render('products/novelties', {
-            booksNovelties : booksNovelties
+            booksNovelties,
+            booksRecommended
         })},
     aboutUs: (req, res) => { res.render('aboutUs')},
 
