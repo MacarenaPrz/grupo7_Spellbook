afec15d0c1ad946cc64af69e1adb4706e89a6c2e
MacarenaPrz <perezrmacarena@gmail.com>
2021-10-01 17:34:34 -0300

    Script de datos

---
+++ spellBook/src/DB_diagram_script/Script datos.sql
@@ -0,0 +1,10 @@
+insert into authors (name) values ('J. K Rowling');
+insert into authors (name) values ('George R. R. Martin');
+insert into authors (name) values ('J. R. R. Tolkien');
+insert into authors (name) values ('Andrzej Sapkowsk');
+insert into authors (name) values ('Jane Austen');
+
+insert into recommended_ages (age) values ('0 - 5');
+insert into recommended_ages (age) values ('6 - 10');
+insert into recommended_ages (age) values ('11 - 17');
+insert into recommended_ages (age) values ('+ 18');
