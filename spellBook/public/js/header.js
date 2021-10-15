0d4560cb33d8bbb1218fe43cd22772d057acfeee
MacarenaPrz <perezrmacarena@gmail.com>
2021-10-15 15:40:19 -0300

    Trabajando en el header

--- spellBook/public/js/header.js
+++ spellBook/public/js/header.js
@@ -1,14 +1,43 @@
-const btn = document.querySelector('.btn');
-const nav = document.querySelector('.nav');
-const links = document.querySelectorAll('.menu__items');
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const searchIcon = document.querySelector('.search-icon');
+const wizard = document.querySelector('.wizard');
+window.addEventListener('load', () => {
 
-btn.addEventListener('click',()=>{
-    let icon = document.getElementById('icon');
-    icon.classList.toggle('fa-bars');
-    nav.classList.toggle('open');
-    links.forEach(link =>{
-        link.classList.toggle('fade');
+    burger.addEventListener('click', () => {
+        document.querySelector('.line1').classList.toggle('l-top');
+        document.querySelector('.line2').classList.toggle('l-center');
+        document.querySelector('.line3').classList.toggle('l-botton');
+        menu.classList.toggle('menu--open');
+        document.querySelector('.card-user').classList.remove('card-open');
+        document.querySelector('.search').classList.remove('search-open');
     });
-    icon.classList.toggle('fa-times');
-});
 
+    searchIcon.addEventListener('click', () => {
+        document.querySelector('.search').classList.toggle('search-open');
+        document.querySelector('.line1').classList.remove('l-top');
+        document.querySelector('.line2').classList.remove('l-center');
+        document.querySelector('.line3').classList.remove('l-botton');
+        menu.classList.remove('menu--open');
+        document.querySelector('.card-user').classList.remove('card-open');
+    });
+
+    wizard.addEventListener('click', () => {
+        document.querySelector('.card-user').classList.toggle('card-open');
+        document.querySelector('.line1').classList.remove('l-top');
+        document.querySelector('.line2').classList.remove('l-center');
+        document.querySelector('.line3').classList.remove('l-botton');
+        menu.classList.remove('menu--open');
+        document.querySelector('.search').classList.remove('search-open');
+    });
+
+    window.onscroll = () => {
+        document.querySelector('.line1').classList.remove('l-top');
+        document.querySelector('.line2').classList.remove('l-center');
+        document.querySelector('.line3').classList.remove('l-botton');
+        menu.classList.remove('menu--open');
+        document.querySelector('.search').classList.remove('search-open');
+        document.querySelector('.card-user').classList.remove('card-open');
+    };
+
+});
