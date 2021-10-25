6043600cbe8691b4a4ead37e94ad83b407760c7a
MacarenaPrz <perezrmacarena@gmail.com>
2021-10-25 00:11:45 -0300

    validaciones front login ok

---
+++ spellBook/public/js/loginValidator.js
@@ -0,0 +1,54 @@
+window.addEventListener("load",()=>{
+    let $email = document.querySelector("#email"),
+    $pass = document.querySelector("#password"),
+    $form = document.querySelector("form"),
+    $emailError = document.querySelector("#emailError"),
+    $passwordError = document.querySelector("#passwordError"),
+    icono = "<i class='fas fa-exclamation-circle'></i>"
+    
+    $email.addEventListener("blur" , () => {
+        switch (true) {
+            case !$email.value.trim():
+                $emailError.innerHTML = `${icono} Debes ingresar un email`
+                $email.style.borderColor = "red"
+                break;
+        
+            default:
+                $emailError.innerHTML = ``
+                $email.style.borderColor = "#C1A1D3"
+                break;
+        }
+    })
+    $pass.addEventListener("blur" , () => {
+        switch (true) {
+            case !$pass.value.trim():
+                $passwordError.innerHTML = `${icono} Debes ingresar una contraseña`
+                $pass.style.borderColor = "red"
+                break;
+        
+            default:
+                $passwordError.innerHTML = ``
+                $pass.style.borderColor = "#C1A1D3"
+                break;
+        }
+    })
+
+    $form.addEventListener("submit", (e) => {
+        let error = false;
+        e.preventDefault();
+        let elementosForm = $form.elements;
+        for (let index = 0; index < elementosForm.length - 1; index++) {
+            if (elementosForm[index].value.trim() == "" ) {
+                elementosForm[index].style.borderColor = "red"
+                //llenar un span con errores
+                
+                error = true
+            }
+            
+        }
+        if (!error) {
+            
+            $form.submit() 
+        }
+    } )
+})
