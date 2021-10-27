0499b3d57b7fa2b1899add7302bfa045af0075f6
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2021-10-27 18:12:37 -0300

    se agregan validaciones del front al signUp

--- spellBook/public/js/signUpValidator.js
+++ spellBook/public/js/signUpValidator.js
@@ -7,10 +7,13 @@ window.addEventListener("load", () => {
     $errorsEmail = document.querySelector("#errorsEmail"),
     $errorsPass = document.querySelector("#errorsPass"),
     $errorsPass2 = document.querySelector("#errorsPass2"),
+    $check = document.querySelector('#check')
+    $checkError = document.querySelector('#checkError')
     $submitButton = document.querySelector(".btn__send"),
-    $form = document.querySelector("form");
+    $form = document.querySelector("#form");
     
     let icono = "<i class='fas fa-exclamation-circle'></i>"
+    let validationsErrors = true
 
     let regExAlpha = /^[a-zA-Z\sñáéíóúü ]*$/,
     regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
@@ -21,14 +24,17 @@ window.addEventListener("load", () => {
             case !$inputName.value.trim():
                 $errorsName.innerHTML = `${icono}El campo nombre es obligatiorio`
                 $inputName.style.borderColor = "red"
+                validationsErrors = false
                 break;
             case !regExAlpha.test($inputName.value):
                 $errorsName.innerHTML = `${icono}Ingresa un nombre valido`
                 $inputName.style.borderColor = "red"
+                validationsErrors = false
                 break;
             default:
                 $inputName.style.borderColor = "#C1A1D3"
                 $errorsName.innerHTML = ""
+                validationsErrors = true
                 break;
         }
     })
@@ -38,14 +44,17 @@ window.addEventListener("load", () => {
             case !$inputEmail.value.trim():
                 $errorsEmail.innerHTML = `${icono} El campo E-mail es obligatorio`
                 $inputEmail.style.borderColor = "red"
+                validationsErrors = false
                 break;
             case !regExEmail.test($inputEmail.value):
                 $errorsEmail.innerHTML = `${icono} Ingresa un E-mail valido`
                 $inputEmail.style.borderColor = "red"
+                validationsErrors = false
                 break;
             default:
                 $inputEmail.style.borderColor = "#C1A1D3"
                 $errorsEmail.innerHTML = ""
+                validationsErrors = true
                 break;
         }
     })
@@ -54,14 +63,17 @@ window.addEventListener("load", () => {
             case !$inputPassword.value.trim():
                 $errorsPass.innerHTML = `${icono} El campo contraseña es obligatorio`
                 $inputPassword.style.borderColor = "red"
+                validationsErrors = false
                 break;
             case !regExPass.test($inputPassword.value):
                 $errorsPass.innerHTML = `${icono} La contraseña debe ser alfanumerica y debe tener 8 caracteres`
                 $inputPassword.style.borderColor = "red"
+                validationsErrors = false
                 break;
             default:
                 $inputPassword.style.borderColor = "#C1A1D3"
                 $errorsPass.innerHTML = ""
+                validationsErrors = true
                 break;
         }
     })
@@ -70,22 +82,33 @@ window.addEventListener("load", () => {
             case !$inputConfirm.value.trim():
                 $errorsPass2.innerHTML = `${icono} Debes reingresar tu contraseña`
                 $inputConfirm.style.borderColor = "red"
+                validationsErrors = false
                 break;
             case $inputPassword.value != $inputConfirm.value:
                 $errorsPass2.innerHTML = `${icono} Las contraseñas no coinciden`
                 $inputPass2.style.borderColor = "red"
+                validationsErrors = false
                 break;
             default:
                 $inputConfirm.style.borderColor = "#C1A1D3"
                 $errorsPass2.innerHTML = ""
+                validationsErrors = true
                 break;
         }
     })
+    $check.addEventListener('click', function (){
+        $check.value = "on"
+        $check.classList.toggle('valid')
+        $check.classList.remove('invalid')
+        $checkError.innerHTML = ""
+    })
+
     $form.addEventListener("submit", (e) => {
         let error = false;
         e.preventDefault();
         let elementosForm = $form.elements;
         for (let index = 0; index < elementosForm.length - 1; index++) {
+            console.log(elementosForm[index].value)
             if (elementosForm[index] == "" ) {
                 elementosForm[index].style.borderColor = "#C1A1D3"
                 //llenar un span con errores
@@ -93,8 +116,15 @@ window.addEventListener("load", () => {
             }
             
         }
-        if (!error) {
-            $form.submit() 
+        if(!$check.checked){
+            $check.classList.add('valid');
+            $checkError.innerHTML = "Debes aceptar los términos y condiciones"
+            error = true
+        }
+
+        if (!error && validationsErrors) {
+            alert("Felicidades ya te registrarte, es hora de iniciar sesión")
+            $form.submit()             
         }
     } )
 
